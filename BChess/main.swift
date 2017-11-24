33ccfc1c919586752b2a1cd667c463903db2fddc
Jean Bovet <jean_bovet@mac.com>
2017-11-23 18:55:14 -0800

    Better processing of cmd-line arguments

--- BChess/main.swift
+++ BChess/main.swift
@@ -14,6 +14,10 @@ extension Analysis.Info {
     var uciInfoMessage: String {
         return "info depth \(depth) time \(time) nodes \(nodeEvaluated) nps \(movesPerSecond) score cp \(evaluation.value) pv \(evaluation.lineInfo)"
     }
+    
+    var uciBestMove: String {
+        return "bestmove \(evaluation.move)"
+    }
 }
 
 class UCI {
@@ -21,15 +25,20 @@ class UCI {
     let log: OSLog
     let minimax = Analysis()
     
+    var async = true
+    
     var xcodeMode = false
     
-    var positionFen = StartPosFEN
+    var board = Board()
     
     init() {
         log = OSLog(subsystem: "ch.arizona-software.BChess", category: "uci")
 
         // Disable output buffering otherwise the GUI won't receive any command
         setbuf(__stdoutp, nil)
+        
+        // Initialize by default with the empty board
+        board = StartPosFEN.board()!
     }
     
     func read() -> String? {
@@ -53,61 +62,131 @@ class UCI {
         }
     }
     
-    func evaluate(fen: String, depth: Int, async: Bool = true) {
-        let parser = FENParser(fen: fen)
-        if let board = parser.parse() {
-            if xcodeMode {
-                print(board)
-            }
-            
-            minimax.analyzing = false
-            let evaluateBlock = {
-                let info = self.minimax.searchBestMove(board: board, color: .white, maxDepth: depth) { info in
-                    // info depth 2 score cp 214 time 1242 nodes 2124 nps 34928 pv e2e4 e7e5 g1f3
-                    // info depth 5 seldepth 29 time 5 nodes 13401 nps 2543 score cp -1 pv b1a3 a7b6 a2a4 a8a4
-                    let message = info.uciInfoMessage
-                    print(message)
-                    if !self.xcodeMode {
-                        os_log("Info: %{public}@", log: self.log, message)
-                    }
-                }
-                
+    func evaluate(board: Board, depth: Int) {
+        if xcodeMode {
+            print(board)
+        }
+        
+        minimax.analyzing = false
+        let evaluateBlock = {
+            let info = self.minimax.searchBestMove(board: board, color: .white, maxDepth: depth) { info in
+                // info depth 2 score cp 214 time 1242 nodes 2124 nps 34928 pv e2e4 e7e5 g1f3
+                // info depth 5 seldepth 29 time 5 nodes 13401 nps 2543 score cp -1 pv b1a3 a7b6 a2a4 a8a4
                 let message = info.uciInfoMessage
                 print(message)
                 if !self.xcodeMode {
-                    os_log("Done: %{public}@", log: self.log, message)
+                    os_log("Info: %{public}@", log: self.log, message)
                 }
             }
             
-            if async {
-                DispatchQueue.global(qos: .userInitiated).async(execute: evaluateBlock)
-            } else {
-                evaluateBlock()
+            // bestmove g1f3
+            let message = info.uciBestMove
+            print(message)
+            if !self.xcodeMode {
+                os_log("Done: %{public}@", log: self.log, message)
             }
-            //        print(board)
-            //
-            //        var lineBoard = board
-            //        for move in evaluation.line {
-            //            lineBoard = lineBoard.move(move: move)
-            //            print(lineBoard)
-            //        }
-            
-//            print("* Best move is \(evaluation.move) with \(evaluation.line) and \(minimax.evaluateCount) moves evaluated.")
+        }
+        
+        if async {
+            DispatchQueue.global(qos: .userInitiated).async(execute: evaluateBlock)
+        } else {
+            evaluateBlock()
         }
     }
 
+    func processCmdPosition(_ tokens: inout [String]) {
+        // position startpos moves e2e4
+        // position fen 8/8/8/1q1k4/8/2P5/1N6/4K3 w - - 0 1 moves c3c4
+        // position fen 8/8/8/1q1k4/8/2P5/1N6/4K3 w - - 0 1
+        let cmd = tokens.removeFirst()
+        
+        if cmd == "startpos" {
+            board = StartPosFEN.board()!
+        } else if cmd == "fen" {
+            let fen = tokens[0...5].joined(separator: " ")
+            board = fen.board()!
+            tokens.removeFirst(6)
+        }
+        
+        // Optional moves
+        if tokens.first == "moves" {
+            processCmdMove(&tokens)
+        }
+    }
+    
+    func processCmdMove(_ tokens: inout [String]) {
+        assert(tokens.removeFirst() == "moves")
+        for moveToken in tokens {
+            assert(moveToken.count == 4)
+            let start = moveToken.startIndex
+            let middle = moveToken.index(start, offsetBy: 1)
+            guard let from = String(moveToken[start...middle]).coordinate else {
+                return
+            }
+            
+            let secondToken = moveToken.index(start, offsetBy: 2)
+            guard let to = String(moveToken[secondToken...]).coordinate else {
+                return
+            }
+            let move = Move(from: from, to: to)
+            board = board.move(move: move)
+        }
+        tokens.removeAll()
+    }
+    
+    func processCmdGo(_ tokens: inout [String]) {
+        // go infinite
+        // go wtime 300000 btime 300000
+        let cmd = tokens.removeFirst()
+        
+        if cmd == "infinite" {
+            evaluate(board: board, depth: Int.max)
+        } else {
+            evaluate(board: board, depth: 3)
+        }
+    }
+    
+    func process(_ tokens: inout [String]) {
+        let cmd = tokens.removeFirst()
+        
+        switch cmd {
+        case "xcode":
+            async = false
+            
+        case "quit":
+            exit(0)
+            
+        case "isready":
+            write("readyok")
+            
+        case "ucinewgame":
+            // New game
+            break
+            
+        case "position":
+            processCmdPosition(&tokens)
+            
+        case "go":
+            processCmdGo(&tokens)
+            
+        case "stop":
+            minimax.analyzing = false
+            
+        default:
+            if xcodeMode {
+                write("Unknown command \(cmd)")
+            } else {
+                os_log("Unknown command: %{public}@", log: log, cmd)
+            }
+        }
+    }
+    
     func run() {
         if CommandLine.arguments.count > 1 {
-            var arg = CommandLine.arguments[1]
-            xcodeMode = arg == "xcode=true"
-            
-            arg = CommandLine.arguments[2]
-            if arg.hasPrefix("fen=") {
-                // Best move is rook to check the black king: 4k3/8/8/8/8/8/2R5/4K3 w - - 0 1
-                // Best move is the pawn to check both the queen and the king: 8/8/8/1q1k4/8/2P5/1N6/4K3 w - - 0 1
-                evaluate(fen: "4k3/8/8/8/8/8/2R5/4K3 w - - 0 1", depth: 7, async: false)
-            } else if arg == "go infinite" {
-                evaluate(fen: StartPosFEN, depth: Int.max, async: false)
+            let arguments = CommandLine.arguments[1...].map { String($0) }
+            for tokens in arguments.split(separator: "||") {
+                var t = tokens.map { String($0) }
+                process(&t)
             }
             exit(0)
         }
@@ -126,42 +205,8 @@ class UCI {
                 // go infinite
                 // stop
                 
-                switch line {
-                case "quit":
-                    exit(0)
-                    break
-                    
-                case "isready":
-                    write("readyok")
-                    break
-                    
-                case "position startpos":
-                    positionFen = StartPosFEN
-                    break
-                    
-                case "go infinite":
-                    evaluate(fen: positionFen, depth: Int.max)
-                    break
-                    
-                case "stop":
-                    minimax.analyzing = false
-                    break
-                    
-                default:
-                    let cmd = "position fen"
-                    if line.hasPrefix(cmd){
-                        // position fen 8/8/8/1q1k4/8/2P5/1N6/4K3 w - - 0 1
-                        let fenIndex = line.index(line.startIndex, offsetBy: cmd.count)
-                        let fen = line[fenIndex...]
-                        positionFen = String(fen)
-                    } else {
-                        if xcodeMode {
-                            write("Unknown command \(line)")
-                        } else {
-                            os_log("Unknown command: %{public}@", log: log, line)
-                        }
-                    }
-                }
+                var tokens = line.split(separator: " ").map { String($0) }
+                process(&tokens)
             }
         }
     }
