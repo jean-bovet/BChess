e396a546c6c737b0e61bf1918440fb92196bc1e1
Jean Bovet <jean_bovet@mac.com>
2017-11-25 19:25:26 -0800

    Add bonus point for pieces that are placed in the center of the board

--- BChess/Evaluation/Evaluate.swift
+++ BChess/Evaluation/Evaluate.swift
@@ -10,6 +10,27 @@ import Foundation
 
 class Evaluate {
     
+    static let scoreMat = 90000
+    
+    static let scorePieceFactor = 100 // Pawn=100, Queen=900
+
+    // Bonus for each piece that is attacking or being attacked
+    static let attackingBonus = 50
+    
+    // Bonus each time the king is in check
+    static let checkingBonus = 100
+
+    static let locationFactor: [Int] = [
+        0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  2,  2,  2,  2,  0,  0,
+        0,  0,  2,  3,  3,  2,  0,  0,
+        0,  0,  2,  3,  3,  2,  0,  0,
+        0,  0,  2,  2,  2,  2,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,
+    ]
+    
     static func evaluate(board: Board) -> Int {
         let generator = MoveGenerator(board: board, color: board.color)
         let moves = generator.generateMoves()
@@ -20,7 +41,7 @@ class Evaluate {
         // Check for mate or draw if there are no moves for this position
         if moves.isEmpty {
             if board.isCheck(color: board.color) {
-                return board.color == .white ? -90000 : 90000
+                return board.color == .white ? -scoreMat : scoreMat
             } else {
                 // TODO Good value to return?
                 return 0
@@ -29,21 +50,21 @@ class Evaluate {
         
         // Note: the value should always be computed from a white point of view
         var value = 0
-        for (piece, _) in board {
+        for (piece, coordinate) in board {
             if piece.isEmpty {
                 continue
             }
             
+            let locFactor = locationFactor[coordinate.rank*Board.size+coordinate.file]
+            let pieceBonus = piece.type.value * locFactor
             if piece.color == .white {
-                value += piece.type.value * 100
+                value += piece.type.value * scorePieceFactor + pieceBonus
             } else {
-                value -= piece.type.value * 100
+                value -= piece.type.value * scorePieceFactor + pieceBonus
             }
         }
         
         // Evaluate the number of pieces being attacked
-        let attackingBonus = 10
-        let checkingBonus = 100
         let whiteIsAttacking = board.color == .white
         for move in moves {
             let targetSquare = board[move.to]
