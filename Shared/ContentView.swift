969132e6179b4e7cfc3899fd60b84f894c84325e
Jean Bovet <jean_bovet@mac.com>
2021-01-09 22:05:12 -0800

    Simplify the code

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -62,19 +62,40 @@ struct LastMoveModifier: ViewModifier {
     }
 }
 
-struct ContentView: View {
-    @Binding var document: BChessUIDocument
-    @State private var selection = Position.empty()
-    @State private var possibleMoves = [FEngineMove]()
-    @State private var lastMove: FEngineMove?
+struct PieceModifier: ViewModifier {
+    
+    let rank: Int
+    let file: Int
+    let pieces: [Piece]
+    
+    func pieceImage(atRank rank: Int, file: Int) -> Image? {
+        return pieces.filter { $0.rank == rank && $0.file == file }.first?.image
+    }
 
-    func backgroundColor(rank: Int, file: Int) -> Color {
-        if rank % 2 == 0 {
-            return file % 2 == 0 ? .gray : .white
-        } else {
-            return file % 2 == 0 ? .white : .gray
+    func body(content: Content) -> some View {
+        let image = pieceImage(atRank: rank, file: file)
+        return content.if(image != nil) { view in
+            view.overlay(image!.resizable())
         }
     }
+}
+
+struct SelectionModifier: ViewModifier {
+    
+    let rank: Int
+    let file: Int
+    let selection: SelectionState
+
+    func body(content: Content) -> some View {
+        return content.if(selection.selected(rank: rank, file: file) || selection.isPossibleMove(rank, file)) { view in
+                view.overlay(Color.yellow.opacity(0.8))
+            }
+    }
+}
+
+struct SelectionState {
+    let selection: Position
+    let possibleMoves: [FEngineMove]
     
     func selected(rank: Int, file: Int) -> Bool {
         return selection.rank == rank && selection.file == file
@@ -88,30 +109,47 @@ struct ContentView: View {
         return possibleMoves.filter { $0.fromFile == file && $0.fromRank == rank || $0.toFile == file && $0.toRank == rank }.first
     }
     
-    func pieceImage(atRank rank: Int, file: Int) -> Image? {
-        return document.pieces.filter { $0.rank == rank && $0.file == file }.first?.image
-    }
+}
 
+struct ContentView: View {
+    @Binding var document: BChessUIDocument
+    @State private var selection = SelectionState(selection: Position.empty(), possibleMoves: [])
+    @State private var lastMove: FEngineMove?
+
+    func backgroundColor(rank: Int, file: Int) -> Color {
+        if rank % 2 == 0 {
+            return file % 2 == 0 ? .gray : .white
+        } else {
+            return file % 2 == 0 ? .white : .gray
+        }
+    }
+    
     func processTap(_ rank: Int, _ file: Int) {
-        if let move = possibleMove(rank, file) {
-            document.engine.move(move.rawMoveValue)
-            document.engine.evaluate { (info, completed) in
-                DispatchQueue.main.async {
-                    if completed {
-                        self.document.engine.move(info.bestMove)
-                        
-                        // TODO: refactor?
-                        let move = FEngineMove()
-                        move.fromFile = info.fromFile
-                        move.fromRank = info.fromRank
-                        move.toFile = info.toFile
-                        move.toRank = info.toRank
-                        move.rawMoveValue = info.bestMove
-                        self.lastMove = move
+        if let move = selection.possibleMove(rank, file) {
+            withAnimation {
+                selection = SelectionState(selection: Position.empty(), possibleMoves: [])
+
+                document.engine.move(move.rawMoveValue)
+                document.engine.evaluate { (info, completed) in
+                    DispatchQueue.main.async {
+                        if completed {
+                            self.document.engine.move(info.bestMove)
+                            
+                            // TODO: refactor?
+                            let move = FEngineMove()
+                            move.fromFile = info.fromFile
+                            move.fromRank = info.fromRank
+                            move.toFile = info.toFile
+                            move.toRank = info.toRank
+                            move.rawMoveValue = info.bestMove
+                            self.lastMove = move
+                        }
                     }
-//                    self.infoChanged?(info)
                 }
             }
+        } else {
+            selection = SelectionState(selection: Position(rank: rank, file: file),
+                                       possibleMoves: document.engine.moves(at: UInt(rank), file: UInt(file)))
         }
     }
     
@@ -120,33 +158,14 @@ struct ContentView: View {
             ForEach((0...7).reversed(), id: \.self) { rank in
                 HStack(spacing: 0) {
                     ForEach((0...7), id: \.self) { file in
-                        if let image = pieceImage(atRank: rank, file: file) {
-                            Rectangle()
-                                .fill(backgroundColor(rank: rank, file: file))
-                                .overlay(image.resizable())
-                                .if(selected(rank: rank, file: file) || isPossibleMove(rank, file)) { view in
-                                    view.overlay(Color.yellow.opacity(0.8))
-                                }
-                                .modifier(LastMoveModifier(rank: rank, file: file, lastMove: lastMove))
-                                .onTapGesture {
-                                    selection = Position(rank: rank, file: file)
-                                    possibleMoves = document.engine.moves(at: UInt(rank), file: UInt(file))
-                                }
-                        } else {
-                            Rectangle()
-                                .fill(backgroundColor(rank: rank, file: file))
-                                .if(isPossibleMove(rank, file)) { view in
-                                    view.overlay(Color.yellow.opacity(0.8))
-                                }
-                                .modifier(LastMoveModifier(rank: rank, file: file, lastMove: lastMove))
-                                .onTapGesture {
-                                    withAnimation {
-                                        processTap(rank, file)
-                                        selection = Position.empty()
-                                        possibleMoves.removeAll()
-                                    }
-                                }
-                        }
+                        Rectangle()
+                            .fill(backgroundColor(rank: rank, file: file))
+                            .modifier(PieceModifier(rank: rank, file: file, pieces: document.pieces))
+                            .modifier(SelectionModifier(rank: rank, file: file, selection: selection))
+                            .modifier(LastMoveModifier(rank: rank, file: file, lastMove: lastMove))
+                            .onTapGesture {
+                                processTap(rank, file)
+                            }
                     }
                 }
             }
