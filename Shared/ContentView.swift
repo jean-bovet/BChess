a712d1aaa3534d1560853c0d262883eaa760cab8
Jean Bovet <jean_bovet@mac.com>
2021-01-10 17:29:47 -0800

    Refactored to support animation

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -62,23 +62,23 @@ struct LastMoveModifier: ViewModifier {
     }
 }
 
-struct PieceModifier: ViewModifier {
-    
-    let rank: Int
-    let file: Int
-    let pieces: [Piece]
-    
-    func pieceImage(atRank rank: Int, file: Int) -> Image? {
-        return pieces.filter { $0.rank == rank && $0.file == file }.first?.image
-    }
-
-    func body(content: Content) -> some View {
-        let image = pieceImage(atRank: rank, file: file)
-        return content.if(image != nil) { view in
-            view.overlay(image!.resizable())
-        }
-    }
-}
+//struct PieceModifier: ViewModifier {
+//
+//    let rank: Int
+//    let file: Int
+//    let pieces: [Piece]
+//
+//    func pieceImage(atRank rank: Int, file: Int) -> Image? {
+//        return pieces.filter { $0.rank == rank && $0.file == file }.first?.image
+//    }
+//
+//    func body(content: Content) -> some View {
+//        let image = pieceImage(atRank: rank, file: file)
+//        return content.if(image != nil) { view in
+//            view.overlay(image!.resizable())
+//        }
+//    }
+//}
 
 struct SelectionModifier: ViewModifier {
     
@@ -136,13 +136,13 @@ struct TopInformationView: View {
 
 struct BottomInformationView: View {
     
-    let info: FEngineInfo?
+    let state: BoardState
 
     let numberFormatter = NumberFormatter()
     let valueFormatter = NumberFormatter()
 
-    init(info: FEngineInfo?) {
-        self.info = info
+    init(state: BoardState) {
+        self.state = state
         
         numberFormatter.numberStyle = .decimal
         numberFormatter.groupingSeparator = ","
@@ -156,7 +156,7 @@ struct BottomInformationView: View {
     }
 
     func value() -> String {
-        guard let info = info else {
+        guard let info = state.info else {
             return " "
         }
         
@@ -171,7 +171,7 @@ struct BottomInformationView: View {
     }
     
     func speed() -> String {
-        guard let info = info else {
+        guard let info = state.info else {
             return " "
         }
 
@@ -188,7 +188,7 @@ struct BottomInformationView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            if let opening = info?.opening {
+            if let opening = state.info?.opening {
                 Text(opening)
             } else {
                 Text(value())
@@ -198,105 +198,15 @@ struct BottomInformationView: View {
     }
 }
 
-struct ContentView: View {
-    @Binding var document: BChessUIDocument
-    
-    @State private var selection = SelectionState(selection: Position.empty(), possibleMoves: [])
-    @State private var lastMove: FEngineMove?
-    
-    @State private var showInfo: Bool = false
-    @State private var info: FEngineInfo?
-    @State private var level: Int = 0
+enum PlayAgainst: Int {
+    case white
+    case black
+    case human
+}
 
-    enum PlayAgainst: Int {
-        case white
-        case black
-        case human
-    }
+struct PositionTransformer {
     
-    @State private var playAgainst: PlayAgainst = .black
-
-    var engine: FEngine {
-        return document.engine
-    }
-    
-    func backgroundColor(rank: Int, file: Int) -> Color {
-        if rank % 2 == 0 {
-            return file % 2 == 0 ? .gray : .white
-        } else {
-            return file % 2 == 0 ? .white : .gray
-        }
-    }
-    
-    func applyLevelSettings() {
-        switch level {
-        case 0:
-            engine.thinkingTime = 2
-        case 1:
-            engine.thinkingTime = 5
-        case 2:
-            engine.thinkingTime = 10
-        case 3:
-            engine.thinkingTime = 15
-        default:
-            engine.thinkingTime = 2
-        }
-    }
-    
-    func processTap(_ rank: Int, _ file: Int) {
-        if let move = selection.possibleMove(rank, file) {
-            withAnimation {
-                selection = SelectionState(selection: Position.empty(), possibleMoves: [])
-                applyLevelSettings()
-                moveEngine(move: move.rawMoveValue)
-                enginePlay()
-            }
-        } else {
-            selection = SelectionState(selection: Position(rank: rank, file: file),
-                                       possibleMoves: engine.moves(at: UInt(rank), file: UInt(file)))
-        }
-    }
-
-    func enginePlay() {
-        engine.evaluate { (info, completed) in
-            DispatchQueue.main.async {
-                if completed {
-                    self.moveEngine(move: info.bestMove)
-                    
-                    // TODO: refactor?
-                    let move = FEngineMove()
-                    move.fromFile = info.fromFile
-                    move.fromRank = info.fromRank
-                    move.toFile = info.toFile
-                    move.toRank = info.toRank
-                    move.rawMoveValue = info.bestMove
-                    self.lastMove = move
-                }
-                self.info = info
-            }
-        }
-    }
-    
-    func moveEngine(move: UInt) {
-        engine.move(move)
-        document.pgn = engine.pgn()
-    }
-    
-    func newGame(playAgainstWhite: Bool) {
-        engine.setFEN(StartPosFEN)
-        if playAgainstWhite {
-            playAgainst = .white
-        } else {
-            playAgainst = .black
-        }
-        if engine.isWhite() && playAgainst == .white {
-            enginePlay()
-        }
-        if !engine.isWhite() && playAgainst == .black {
-            enginePlay()
-        }
-        selection = SelectionState(selection: Position.empty(), possibleMoves: [])
-    }
+    let playAgainst: PlayAgainst
 
     func actualRank(_ rank: Int) -> Int {
         if playAgainst == .white {
@@ -313,33 +223,214 @@ struct ContentView: View {
             return file
         }
     }
+}
+
+struct SquareView: View {
+        
+    let image: Image?
+    
+    var body: some View {
+        if let image = image {
+            image.resizable()
+        } else {
+            // The contentShape() is needed to define the hit
+            // testing area because otherwise with opacity(0),
+            // the first Rectangle is not tappable at all.
+            Rectangle().opacity(0).contentShape(Rectangle())
+        }
+    }
+    
+}
+
+struct BoardView: View {
+    
+    @ObservedObject var state: BoardState
+
+    let engine: FEngine
+    let pieces: [Piece]
+
+    func piece(atRank rank: Int, file: Int) -> Piece? {
+        return pieces.filter { $0.rank == rank && $0.file == file }.first
+    }
+
+    func applyLevelSettings() {
+        switch state.level {
+        case 0:
+            engine.thinkingTime = 2
+        case 1:
+            engine.thinkingTime = 5
+        case 2:
+            engine.thinkingTime = 10
+        case 3:
+            engine.thinkingTime = 15
+        default:
+            engine.thinkingTime = 2
+        }
+    }
+    
+    func enginePlay() {
+        engine.evaluate { (info, completed) in
+            DispatchQueue.main.async {
+                withAnimation {
+                if completed {
+                    self.moveEngine(move: info.bestMove)
+
+                    // TODO: refactor?
+                    let move = FEngineMove()
+                    move.fromFile = info.fromFile
+                    move.fromRank = info.fromRank
+                    move.toFile = info.toFile
+                    move.toRank = info.toRank
+                    move.rawMoveValue = info.bestMove
+                    state.lastMove = move
+                }
+                state.info = info
+                }
+            }
+        }
+    }
+    
+    func moveEngine(move: UInt) {
+        engine.move(move)
+        // TODO
+//        document.pgn = engine.pgn()
+    }
+    
+    func processTap(_ rank: Int, _ file: Int) {
+        if let move = state.selection.possibleMove(rank, file) {
+            withAnimation {
+                state.selection = SelectionState(selection: Position.empty(), possibleMoves: [])
+                applyLevelSettings()
+                moveEngine(move: move.rawMoveValue)
+                enginePlay()
+            }
+        } else {
+            state.selection = SelectionState(selection: Position(rank: rank, file: file),
+                                       possibleMoves: engine.moves(at: UInt(rank), file: UInt(file)))
+        }
+    }
+
+    func board(withPieces pieces: [Piece]) -> [Square] {
+        var squares = [Square]()
+        for rank in 0...7 {
+            for file in 0...7 {
+                let piece = pieces.piece(atRank: rank, file: file)
+                let square = Square(rank: rank, file: file, piece: piece)
+                squares.append(square)
+            }
+        }
+        return squares
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let minSize: CGFloat = min(geometry.size.width, geometry.size.height)
+            let xOffset: CGFloat = (geometry.size.width - minSize) / 2
+            let yOffset: CGFloat = (geometry.size.height - minSize) / 2
+            let squareSize: CGFloat = minSize / 8
+            let b: [Square] = board(withPieces: pieces)
+            ForEach(b) { square in
+                let r: Int = square.rank
+                let f: Int = square.file
+                let x: CGFloat = CGFloat(f) * squareSize + xOffset
+                let y: CGFloat = CGFloat(7 - r) * squareSize + yOffset
+                SquareView(image: square.piece?.image)
+                    .frame(width: squareSize, height: squareSize)
+                    .offset(x: x,
+                            y: y)
+                    .onTapGesture {
+                        processTap(r, f)
+                    }
+            }
+        }
+    }
+}
+
+class BoardState: ObservableObject {
+    @Published var selection = SelectionState(selection: Position.empty(), possibleMoves: [])
+    @Published var lastMove: FEngineMove? = nil
+    @Published var info: FEngineInfo? = nil
+    @Published var level: Int = 0
+    @Published var playAgainst: PlayAgainst = .black
+}
+
+struct Square: Identifiable {
+    var id: String {
+        return piece?.name ?? "\(UUID())"
+    }
+    let rank: Int
+    let file: Int
+    let piece: Piece?
+}
+
+extension Array where Element == Piece {
+    func piece(atRank rank: Int, file: Int) -> Piece? {
+        return filter { $0.rank == rank && $0.file == file }.first
+    }
+}
+
+struct ContentView: View {
+    @Binding var document: BChessUIDocument
+    
+    @ObservedObject private var state = BoardState()
+        
+    @State private var showInfo: Bool = false
+
+    var engine: FEngine {
+        return document.engine
+    }
+    
+    func backgroundColor(rank: Int, file: Int) -> Color {
+        if rank % 2 == 0 {
+            return file % 2 == 0 ? .gray : .white
+        } else {
+            return file % 2 == 0 ? .white : .gray
+        }
+    }
+    
+    func newGame(playAgainstWhite: Bool) {
+//        engine.setFEN(StartPosFEN)
+//        if playAgainstWhite {
+//            playAgainst = .white
+//        } else {
+//            playAgainst = .black
+//        }
+//        if engine.isWhite() && playAgainst == .white {
+//            enginePlay()
+//        }
+//        if !engine.isWhite() && playAgainst == .black {
+//            enginePlay()
+//        }
+//        selection = SelectionState(selection: Position.empty(), possibleMoves: [])
+    }
 
     var body: some View {
         VStack(alignment: .leading) {
-            TopInformationView(info: info, engine: engine)
+            TopInformationView(info: state.info, engine: engine)
             
-            VStack(spacing: 0) {
-                ForEach((0...7).reversed(), id: \.self) { rank in
-                    let r = actualFile(rank)
-                    HStack(spacing: 0) {
-                        ForEach((0...7), id: \.self) { file in
-                            let f = actualFile(file)
-                            Rectangle()
-                                .fill(backgroundColor(rank: r, file: f))
-                                .modifier(PieceModifier(rank: r, file: f, pieces: document.pieces))
-                                .modifier(SelectionModifier(rank: r, file: f, selection: selection))
-                                .modifier(LastMoveModifier(rank: r, file: f, lastMove: lastMove))
-                                .onTapGesture {
-                                    processTap(r, f)
-                                }
+            ZStack {
+                VStack(spacing: 0) {
+                    let pt = PositionTransformer(playAgainst: state.playAgainst)
+                    ForEach((0...7).reversed(), id: \.self) { rank in
+                        let r = pt.actualFile(rank)
+                        HStack(spacing: 0) {
+                            ForEach((0...7), id: \.self) { file in
+                                let f = pt.actualFile(file)
+                                Rectangle()
+                                    .fill(backgroundColor(rank: r, file: f))
+                                    .modifier(LastMoveModifier(rank: r, file: f, lastMove: state.lastMove))
+                                    .modifier(SelectionModifier(rank: r, file: f, selection: state.selection))
+                            }
                         }
                     }
                 }
+                .aspectRatio(1.0, contentMode: .fit)
+
+                BoardView(state: state, engine: engine, pieces: document.pieces)
             }
-            .aspectRatio(1.0, contentMode: .fit)
             
             if (showInfo) {
-                BottomInformationView(info: info)
+                BottomInformationView(state: state)
             }
         }
         .padding()
@@ -358,7 +449,7 @@ struct ContentView: View {
 
                     Divider()
                     
-                    Picker(selection: $level, label: Text("Level")) {
+                    Picker(selection: $state.level, label: Text("Level")) {
                         Text("2 seconds").tag(0)
                         Text("5 seconds").tag(1)
                         Text("10 seconds").tag(2)
